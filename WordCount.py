e9b943cb9f96bbcbfe0bcf858aa0cbeca508fcc1
Stefano Palazzo <s.palazzo@alumno.um.edu.ar>
2023-04-11 00:02:51 -0300

    Contador de palabras

---
+++ WordCount.py
@@ -0,0 +1,16 @@
+import unittest
+
+diccionario = {}
+
+def count_words(frase):
+    palabras = frase.split(' ')  # Separa el texto en palabras
+    for palabra in palabras:
+      if palabra in diccionario:
+        diccionario[palabra] += 1
+      else:
+        diccionario[palabra] = 1
+    return(diccionario)
+
+
+if __name__ == '__main__':
+    unittest.main()
