830fa8ce62890c0afdade2d2a5494f719f132b86
Stefano Palazzo <s.palazzo@alumno.um.edu.ar>
2023-04-11 07:59:21 -0300

    3 tests

--- my_tests.py
+++ my_tests.py
@@ -4,8 +4,16 @@ from WordCount import count_words
 class TestCountLetters(unittest.TestCase):
 
     def test_uno(self):
+        result = count_words('hola como')
+        self.assertEqual(result, {'hola': 1, 'como' : 1})
+
+    def test_dos(self):
         result = count_words('hola')
         self.assertEqual(result, {'hola': 1})
+
+    def test_tres(self):
+        result = count_words('hola como estas hola')
+        self.assertEqual(result, {'hola': 2, 'como' : 1, 'estas' : 1})
 
 
 if __name__ == '__main__':
